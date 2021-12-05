0e79f58995f2c11aab772925dbb0a0aace8d33fd
Hamza Hassan Khan <65834208+hamzakhan0077@users.noreply.github.com>
2021-12-05 21:07:29 +0000

    Do Tests

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.javaasg3v11 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.junit.jupiter.api;
 
 
     opens com.example.javaasg3v11 to javafx.fxml;
